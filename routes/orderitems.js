e8c6bf155f68b98a70db8857761d24925f55d2d5
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-03-09 08:59:22 +0530

    Update

--- routes/orderitems.js
+++ routes/orderitems.js
@@ -3,45 +3,34 @@ const router = require("express").Router();
 const Student = require("../model/student");
 
 router.post("/:StudentID/:DayID", (req, res) => {
-  Student.findOneAndUpdate(
-    { _id: req.params.StudentID, "history._id": req.params.DayID },
-    { $set: { "history.$.item": req.body } }
-  )
-    .then(() => {
-      Student.findById(req.params.StudentID)
-        .then(data => {
-          let balance = data.balance;
-          let cost = 0;
-          for (let day of data.history) {
-            if (day._id.toString() === req.params.DayID) {
-              for (let items of day.item) {
-                cost = cost + items.price * items.quantity;
-              }
-            }
-          }
-          balance = balance - cost;
-          Student.findByIdAndUpdate(
-            req.params.StudentID,
-            { $set: { balance: balance } },
-            err => {
-              if (err) {
-                res.json(err);
-              } else {
-                res.json({
-                  Status: "Successfully Updated Balance",
-                  "Balance Remaining": balance
-                });
-              }
-            }
-          );
-        })
-        .catch(err => {
-          res.json(err);
-        });
-    })
-    .catch(err => {
-      res.json(err);
-    });
+  Student.findById(req.params.StudentID).then(data => {
+    let balance = data.balance;
+    let cost = 0;
+    let p = [...data.history[0].item];
+    for (let day of data.history) {
+      if (day._id.toString() === req.params.DayID) {
+        console.log(day);
+        for (let items of day.item) {
+          //console.log(items);
+          cost = cost + items.price * items.quantity;
+        }
+      }
+    }
+    for (let r of req.body) {
+      p.push(r);
+    }
+    balance = balance - cost;
+    Student.findOneAndUpdate(
+      { _id: req.params.StudentID, "history._id": req.params.DayID },
+      { $set: { "history.$.item": p, balance: balance } }
+    )
+      .then(() => {
+        res.json({ Status: "Updated Balance" });
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
 });
 
 module.exports = router;

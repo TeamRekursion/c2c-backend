e7412c0dc10d5dd37534c3263a78ae50fa580535
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-03-09 09:34:53 +0530

    Update

--- routes/orderitems.js
+++ routes/orderitems.js
@@ -4,23 +4,20 @@ const Student = require("../model/student");
 
 router.post("/:StudentID/:DayID", (req, res) => {
   Student.findById(req.params.StudentID).then(data => {
-    let balance = data.balance;
     let cost = 0;
     let p = [...data.history[0].item];
-    for (let day of data.history) {
-      if (day._id.toString() === req.params.DayID) {
-        for (let items of day.item) {
-          cost = cost + items.price * items.quantity;
-        }
+    for (let items of req.body) {
+      if (items.price && items.quantity) {
+        cost = cost + items.price * items.quantity;
       }
     }
     for (let r of req.body) {
       p.push(r);
     }
-    balance = balance - cost;
+    let balance = data.balance - cost;
     Student.findOneAndUpdate(
       { _id: req.params.StudentID, "history._id": req.params.DayID },
-      { $set: { "history.$.item": p, balance: balance } }
+      { $set: { "history.$.item": p }, balance: balance }
     )
       .then(() => {
         res.json({ Status: "Updated Balance" });
